60fcfecaaeb7b75668361843eb1ffa3a3f2a85d3
Moh. Romadhani Firdaus <mrfirdaus123@gmail.com>
2018-11-21 19:28:21 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,46 @@
+public class Mahasiswa
+{
+    private int nrp,nohp,usia;
+    private String nama,alamat,email;
+    
+    // instance variables - replace the example below with your own
+    public Mahasiswa(String nama)
+    {
+        // initialise instance variables
+        
+    }
+    public Mahasiswa(int nrp)
+    {
+        // initialise instance variables
+        
+    }
+    public Mahasiswa(String nama,int nrp)
+    {
+        // initialise instance variables
+        
+    }
+    public Mahasiswa(String nama,int nrp,int usia)
+    {
+        // initialise instance variables
+        
+    }
+    public Mahasiswa(String nama,int nrp,int usia,String alamat)
+    {
+        // initialise instance variables
+        
+    }
+    public Mahasiswa(String nama,int nrp,int usia,String alamat,int nohp)
+    {
+        // initialise instance variables
+        
+    }
+    public Mahasiswa(String nama,int nrp,int usia,String alamat,int nohp,String email)
+    {
+        // initialise instance variables
+        
+    }
+    public static void main(){
+    
+    }
+    
+}
